e6b8dc45f4531326a0411fd56dbea374174654ad
Aldihardi <mochamadaldihardiansyah@gmail.com>
2022-05-24 00:37:28 +0700

    UTS PEMOGRAMAN WEB

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="https://ecs7.tokopedia.net/img/cache/200-square/VqbcmM/2020/10/26/7b2558d7-de18-4867-95b9-68821e0f8bb5.jpg.webp" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">MSI PRESTIGE 14 A11SCX [9S7-14C413-230] i7-1185G7 512GB GTX1650 4GB</h5>
+              <p class="card-text">Rp19.999.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="https://ecs7.tokopedia.net/img/cache/200-square/VqbcmM/2020/10/26/5180a295-86fa-4faa-9401-798086606496.jpg.webp" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">MSI Prestige 14 A11SCX [9S7-14C412-232] i7-1185G7 16GB 1TB GTX1650 4GB</h5>
+              <p class="card-text">Rp19.999.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+            <img src="https://ecs7.tokopedia.net/img/cache/200-square/VqbcmM/2020/10/26/9e07b4aa-59a7-4a0f-8b50-5950a8ecb09f.png.webp" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">MSI PRESTIGE 15 A11SCX [9S7-16S612-234] i7-1185G7 16GB 1TB GTX1650 4GB</h5>
+              <p class="card-text">Rp21.999.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-sm">
+          <div class="card" style="width: 18rem;">
+              <img src="https://ecs7.tokopedia.net/img/cache/200-square/VqbcmM/2020/10/26/c61369df-556e-4783-b47d-3abc06c30328.png.webp" class="card-img-top" alt="gambar laptop">
+              <div class="card-body">
+                <h5 class="card-title">MSI Modern 15 A11SB [9S7-155226-070] i7-1165G7 16GB 512GB MX450 2GB</h5>
+                <p class="card-text">Rp19.999.000</p>
+                <a href="#" class="btn btn-primary">Beli</a>
+              </div>
+            </div>
+        </div>
+
+</div>
+@endsection
